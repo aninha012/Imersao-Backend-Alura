959dc2289e405103a7cf89ef2e968aacf054cf20
ana <anarluisar12@gmail.com>
2024-11-23 22:45:06 -0300

    aula-05

--- instalike-back/src/controllers/postsController.js
+++ instalike-back/src/controllers/postsController.js
@@ -1,5 +1,6 @@
-import {getTodosPosts, criarPost} from "../Model/postsModel.js";
+import {getTodosPosts, criarPost, atualizarPost} from "../Model/postsModel.js";
 import fs from "fs";
+import gerarDescricaoComGemini from "../services/geminiService.js"
 
 export async function listarPosts(req, res) {
     // Obtém todos os posts
@@ -9,7 +10,7 @@ export async function listarPosts(req, res) {
 }
     
 export async function postarNovoPost(req, res) {
-    const novoPost = req.body 
+    const novoPost = req.body; 
     try {
         const postCriado = await criarPost(novoPost);
         res.status(200).json(postCriado);
@@ -34,5 +35,26 @@ export async function uploadImagem(req, res) {
     } catch(erro) {
         console.error(erro.message);
         res.status(500).json({"Erro":"Falha na requisição"})
+    }
+}
+
+export async function atualizarNovoPost(req, res) {
+    const id = req.params.id;
+    const urlImagem = `http://localhost:3000/${id}.png`
+    try {
+        const imgBuffer = fs.readFileSync(`uploads/${id}.png`)
+        const descricao = await gerarDescricaoComGemini(imgBuffer)
+
+        const post = {
+            imgUrl: urlImagem,
+            descricao: descricao,
+            alt: req.body.alt
+        }
+
+        const postCriado = await atualizarPost(id, post);
+        res.status(200).json(postCriado);  
+    } catch(erro) {
+        console.error(erro.message);
+        res.status(500).json({"Erro":"Falha na requisição"});
     }
 }
--- instalike-back/src/routes/postsRoutes.js
+++ instalike-back/src/routes/postsRoutes.js
@@ -1,6 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts, postarNovoPost, uploadImagem } from "../controllers/postsController.js";
+import { listarPosts, postarNovoPost, uploadImagem, atualizarNovoPost } from "../controllers/postsController.js";
+import cors from "cors";
+
+const corsOptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus: 200
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -18,11 +24,14 @@ const upload = multer({ dest: "./uploads" , storage})
 const routes = (app) => {
     // Habilita o parser JSON para o Express
     app.use(express.json());
+    app.use(cors(corsOptions))
     // Rota GET para obter todos os posts
     app.get("/posts", listarPosts);
     // Rota para criar um post
     app.post("/posts", postarNovoPost)
     app.post("/upload", upload.single("imagem"), uploadImagem)
+    
+    app.put("/upload/:id", atualizarNovoPost)
 }
  
 export default routes;
